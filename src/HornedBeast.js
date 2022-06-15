4d11b46e74ef4ced3c1331312c6197ec1fc99e6e
Scottie <scott.c.houghton@gmail.com>
2022-06-14 18:10:00 -0700

    Finished Feature #1

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,11 +1,16 @@
 import React from 'react';
+// import Button from 'react-bootstrap/Button';
 
 class HornedBeast extends React.Component {
   render () {
     return (
       <article>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.title} title={this.props.description}/>
+        <img
+          src={this.props.imageUrl}
+          alt={this.props.title}
+          title={this.props.description}
+        />
         <p>{this.props.description}</p>
       </article>
     )
